81e7e6ccce962dbde875e75840496a3fa99f8a70
felix <lechat-f@hotmail.com>
2018-09-21 19:49:16 -0700

    is_file

--- srcs/process_files.c
+++ srcs/process_files.c
@@ -1,10 +1,20 @@
 #include "ft_ls.h"
 
+int	is_file(const char *path)
+{
+	struct stat	stats;
+
+	stat(path, &stats);
+	if (S_ISREG(stats.st_mode))
+		return (1);
+	return (0);
+}
+
 t_paths	*process_files(t_paths *args, t_flags flags)
 {
-	t_paths	*files;
-	t_paths	*prev;
-	t_paths	*tmp;
+	t_paths		*files;
+	t_paths		*prev;
+	t_paths		*tmp;
 
 	files = NULL;
 	prev = NULL;
